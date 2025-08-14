8e724750852a76f5fa2828526e33e9c881cf4e19
Alisher Aqwa <aqwaryss@gmail.com>
2025-08-14 21:16:11 +0500

    Insert documents to collection learned

--- python/config.py
+++ python/config.py
@@ -7,7 +7,7 @@ load_dotenv('../.env')
 class Config:
     MONGO_ROOT_USERNAME = os.getenv('MONGO_ROOT_USERNAME')
     MONGO_ROOT_PASSWORD = os.getenv('MONGO_ROOT_PASSWORD')
-    MONGO_URL = os.getenv('MONGO_URL')
+    MONGO_HOST = os.getenv('MONGO_HOST')
 
-    REMOTE_URI =  f"mongodb+srv://{MONGO_ROOT_USERNAME}:{MONGO_ROOT_PASSWORD}@{MONGO_URL}/?retryWrites=true&w=majority&appName=Cluster0"
+    REMOTE_URI =  f"mongodb+srv://{MONGO_ROOT_USERNAME}:{MONGO_ROOT_PASSWORD}@{MONGO_HOST}/?retryWrites=true&w=majority&appName=Cluster0"
     LOCAL_URI = f"mongodb://{MONGO_ROOT_USERNAME}:{MONGO_ROOT_PASSWORD}@localhost:27017/"
--- python/crud-insert.py
+++ python/crud-insert.py
@@ -9,6 +9,14 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
     db = client.sample_guides
     coll = db.comets
+    docs = [
+        {"name": "Halley's Comet", "officialName": "1P/Halley", "orbitalPeriod": 75, "radius": 3.4175, "mass": 2.2e14},
+        {"name": "Wild2", "officialName": "81P/Wild", "orbitalPeriod": 6.41, "radius": 1.5534, "mass": 2.3e13},
+        {"name": "Comet Hyakutake", "officialName": "C/1996 B2", "orbitalPeriod": 17000, "radius": 0.77671, "mass": 8.8e12},
+    ]
+
+    result = coll.insert_many(docs)
+    print(result.inserted_ids)
 
 except Exception as e:
     print(e)
